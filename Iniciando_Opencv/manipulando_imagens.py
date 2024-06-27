341f2f58236b10590d56a5d1a7ab793e2d8a2599
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-06-27 20:57:25 -0300

    Criação do projeto e inserido primeiro codigo com Opencv

---
+++ Iniciando_Opencv/manipulando_imagens.py
@@ -0,0 +1,9 @@
+import cv2
+
+img = cv2.imread(r'C:\Users\thiag\Desktop\Deep_Learning_Projects\Iniciando_Opencv\Midias\farol.jpg') #LER IMAGEM
+imgCinza = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+print(img.shape) #MOSTRA O TAMANHO DA IMAGEM
+print(imgCinza) #MOSTRA OS CODIGOS DOS PIXELS
+cv2.imshow('imagem', img) #MOSTRAR IMAGEM
+cv2.imshow('Imagem Cinza', imgCinza)
+cv2.waitKey(0) #MANTER IMAGEM ABERTA
