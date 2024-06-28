1218611863b275a13e1abddbc5524f325cf19022
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-06-27 23:11:28 -0300

    Adicionado manipulação de video

---
+++ Iniciando_Opencv/manipulando_videos.py
@@ -0,0 +1,10 @@
+import cv2
+
+video = cv2.VideoCapture(r'C:\Users\thiag\Desktop\Deep_Learning_Projects\Iniciando_Opencv\Midias\runners.mp4')
+
+while True:
+    check, img = video.read()
+    print(img.shape)
+    imgRedin = cv2.resize(img, (640, 420))
+    cv2.imshow('Video', imgRedin)
+    cv2.waitKey(20)
