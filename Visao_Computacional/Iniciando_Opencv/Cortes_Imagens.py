4f7f6c094532467ee5adc85993f89235cd5041b6
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-07-15 19:16:03 -0300

    Adicionado novo codigo de recorte de imagens

---
+++ Visao_Computacional/Iniciando_Opencv/Cortes_Imagens.py
@@ -0,0 +1,26 @@
+import cv2
+
+# UTILIZANDO SELEÇÃO DA IMAGEM
+img = cv2.imread(r'C:\Users\thiag\Desktop\Deep_Learning_Projects\Visao_Computacional\Iniciando_Opencv\Midias\farol.jpg') #LER IMAGEM
+dim = cv2.selectROI("SELECIONE A AREA DE RECORTE", img, False)
+
+print(dim)
+valor1 = int(dim[0])
+valor2 = int(dim[1])
+valor3 = int(dim[2])
+valor4 = int(dim[3])
+
+
+recorte = img[valor2:valor2+valor4, valor1:valor1+valor3]
+cv2.imshow('Recorte', recorte) #MOSTRAR IMAGEM
+cv2.waitKey(0)
+
+
+
+
+# UTILIZANDO COORDENADAS PRONTAS
+# recorte = img[310:520, 120:420]
+# cv2.imshow('imagem', img) #MOSTRAR IMAGEM
+# cv2.imshow('recorte', recorte) #MOSTRAR IMAGEM
+# cv2.waitKey(0) #MANTER IMAGEM ABERTA
+
