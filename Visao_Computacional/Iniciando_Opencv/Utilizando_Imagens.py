21d1dbe9a630005d757df5315e3883f175041dc2
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2024-07-15 18:59:10 -0300

    Corrigido caminho dos arquivos

--- Visao_Computacional/Iniciando_Opencv/Utilizando_Imagens.py
+++ Visao_Computacional/Iniciando_Opencv/Utilizando_Imagens.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread(r'C:\Users\thiag\Desktop\Deep_Learning_Projects\Iniciando_Opencv\Midias\farol.jpg') #LER IMAGEM
+img = cv2.imread(r'C:\Users\thiag\Desktop\Deep_Learning_Projects\Visao_Computacional\Iniciando_Opencv\Midias\farol.jpg') #LER IMAGEM
 imgCinza = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 print(img.shape) #MOSTRA O TAMANHO DA IMAGEM
 print(imgCinza) #MOSTRA OS CODIGOS DOS PIXELS
--- Visao_Computacional/Iniciando_Opencv/Utilizando_Videos.py
+++ Visao_Computacional/Iniciando_Opencv/Utilizando_Videos.py
@@ -1,6 +1,6 @@
 import cv2
 
-video = cv2.VideoCapture(r'C:\Users\thiag\Desktop\Deep_Learning_Projects\Iniciando_Opencv\Midias\runners.mp4')
+video = cv2.VideoCapture(r'C:\Users\thiag\Desktop\Deep_Learning_Projects\Visao_Computacional\Iniciando_Opencv\Midias\runners.mp4')
 
 while True:
     check, img = video.read()
